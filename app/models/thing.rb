5b7a16b74ce0a5607b01ae2b49f3823ce6dcf559
Louis Simoneau <simoneau.louis@gmail.com>
2013-02-06 16:42:24 +1100

    first validation

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -2,4 +2,6 @@ class Thing < ActiveRecord::Base
   attr_accessible :description, :expires_at, :latitude, :longitude, :title, :image
 
   mount_uploader :image, ImageUploader
+
+  validates_presence_of :title
 end
