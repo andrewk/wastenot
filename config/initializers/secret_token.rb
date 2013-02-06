62748c2ba79a2a5f556e45d7bf60321c4c30a38d
Louis Simoneau <simoneau.louis@gmail.com>
2013-02-06 12:15:50 +1100

    Rails up in this mother

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wastenot::Application.config.secret_token = '6855347b1822484beb6f58b427ef79898dfbd50bceb82740688b6e82dccde8564b071a242d6b99a803c9f6d1826b8369623ce8740a5abe8cc0e84c6679673485'
